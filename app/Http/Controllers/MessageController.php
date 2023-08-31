0ec7e1011168425ce30825e2f72bff481020e4b1
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-31 23:01:42 +0500

    Display messages with Web Socket Pusher (backend)

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\StoreMessageEvent;
 use App\Http\Requests\Message\StoreRequest;
 use App\Http\Resources\Message\MessageResource;
 use App\Models\Message;
@@ -21,6 +22,10 @@ class MessageController extends Controller
         $data = $request->validated();
          $message = Message::create($data);
 
+         event(new StoreMessageEvent($message));
+
          return MessageResource::make($message)->resolve();
     }
+
+
 }
