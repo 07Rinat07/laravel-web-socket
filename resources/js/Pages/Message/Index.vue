2304f8961a6f8d4ba54288f09236a661f8ac1964
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-31 15:17:34 +0500

    Creating a page, controller, route for the posts page

---
+++ resources/js/Pages/Message/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="w-1/2 mx-auto py-6">
+        <div class="mb-4">
+            Messages
+        </div>
+        <div>
+            <div class="mb-4">
+                <input type="text" class="rounded-full border-gray-400" placeholder="your message">
+            </div>
+            <div class="mb-4">
+                <a href="#" class="rounded-lg block w-40 bg-sky-400 text-white text-center py-2">Sent</a>
+            </div>
+       </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Index"
+}
+</script>
+
+<style scoped>
+
+</style>
