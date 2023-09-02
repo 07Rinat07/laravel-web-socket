8f79738adc3a8cb5a86d3a159ad48e892cf916f6
Rinat Sarmuldin <ura07srr@gmail.com>
2023-09-02 23:55:55 +0500

    Styling the User Page and Adding a SendLike Button"

--- resources/js/Pages/User/Show.vue
+++ resources/js/Pages/User/Show.vue
@@ -1,7 +1,13 @@
 <template>
-<div>
-    <div class="p-6 w-1/3 mx-auto">
+<div class="p-6 w-1/3 mx-auto" >
+    <div class="mb-4">
         User {{user.name}}
+    </div>
+    <div class="mb-4">
+        <a href="#"  class="rounded-lg block w-40 bg-sky-400 text-white text-center py-2">Send like</a>
+    </div>
+    <div v-if="like_str">
+        {{like_str}}
     </div>
 </div>
 </template>
@@ -14,7 +20,13 @@ export default {
 
     props: [
         'user'
-    ]
+    ],
+
+    data() {
+        return {
+            like_str: ''
+        }
+    }
 }
 </script>
 
--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/messages', [\App\Http\Controllers\MessageController::class, 'index']);
     Route::post('/messages', [\App\Http\Controllers\MessageController::class, 'store']);
 
-    Route::get('/users/{user}', [\App\Http\Controllers\UserController::class,
-         'show']);
+    Route::get('/users/{user}', [\App\Http\Controllers\UserController::class, 'show']);
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
