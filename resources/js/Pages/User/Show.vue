5017a78e57650be71f914c64b9e559a49cacf566
Rinat Sarmuldin <ura07srr@gmail.com>
2023-09-02 13:10:26 +0500

    User page, route, controller for private channel topics

---
+++ resources/js/Pages/User/Show.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <div class="p-6 w-1/3 mx-auto">
+        User {{user.name}}
+    </div>
+</div>
+</template>
+
+
+
+<script>
+export default {
+    name: "Show",
+
+    props: [
+        'user'
+    ]
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
