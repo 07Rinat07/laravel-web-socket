0f4d0a980c4ffe817e6c488d85a5692e319c0ab8
Rinat Sarmuldin <ura07srr@gmail.com>
2023-09-03 22:09:32 +0500

    Using Workstations with Web Sockets

--- app/Events/StoreMessageEvent.php
+++ app/Events/StoreMessageEvent.php
@@ -6,13 +6,11 @@ use App\Http\Resources\Message\MessageResource;
 use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class StoreMessageEvent implements ShouldBroadcast
+class StoreMessageEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
