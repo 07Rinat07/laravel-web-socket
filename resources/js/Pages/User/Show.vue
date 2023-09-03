f00767d1c209a4a88b0c8bbfc2dbd421791d2121
Rinat Sarmuldin <ura07srr@gmail.com>
2023-09-03 18:07:56 +0500

    Rendering an alert on the client

--- resources/js/Pages/User/Show.vue
+++ resources/js/Pages/User/Show.vue
@@ -28,6 +28,13 @@ export default {
         }
     },
 
+    created() {
+        window.Echo.channel(`send_like_${this.$page.props.auth.user.id}`)
+            .listen('.send_like', res => {
+                this.like_str = res.like_str
+            })
+    },
+
     methods: {
         sendLike() {
             axios.post(`/users/${this.user.id}`, {from_id: this.$page.props.auth.user.id})
